08b223ddb5c218c273254e1aab54a628c867a554
kolyayurev <kolyayurew@gmail.com>
2021-02-15 16:38:14 +0300

    save

--- resources/assets/js/element-ui.js
+++ resources/assets/js/element-ui.js
@@ -1,21 +1,3 @@
-import { 
-    Col,Row,
-    Form,FormItem,Input,Button,
-    Table,TableColumn,
-    Card,Dialog,Collapse, CollapseItem,
-    Loading,Notification } from 'element-ui';
-Vue.use(Col)
-Vue.use(Row)
-Vue.use(Form)
-Vue.use(FormItem)
-Vue.use(Input)
-Vue.use(Button)
-Vue.use(Table)
-Vue.use(TableColumn)
-Vue.use(Card)
-Vue.use(Dialog)
-Vue.use(Collapse)
-Vue.use(CollapseItem)
-Vue.use(Loading.directive);
-Vue.prototype.$loading = Loading.service;
-Vue.prototype.$notify = Notification
+import Element from 'element-ui';
+Vue.use(Element)
+
