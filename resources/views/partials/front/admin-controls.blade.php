1f0bd9bd990ecec5fbf6d61c487273f3e768cec4
kolyayurev <kolyayurew@gmail.com>
2021-08-10 17:58:11 +0300

    save

--- resources/views/partials/front/admin-controls.blade.php
+++ resources/views/partials/front/admin-controls.blade.php
@@ -1,20 +1,20 @@
 @if(config('voyager.controls_panel.enable') && auth()->check() && auth()->user()->can('browse_admin'))
     @php
+        Voyager::addControlButton([
+                    'url'=>route('voyager.dashboard'),
+                    'color'=>config('voyager.primary_color'),
+                    'fontColor'=>'white',
+                    'icon'=>'home',
+                    'title'=>'Админ панель'
+                ]);
         $buttons = Voyager::getControlButtons();
+        
     @endphp
     @if ($buttons->count())
     <div class="admin-controls {{ config('voyager.controls_panel.position') }} ">
         
         <button class="admin-controls__switch {{ Cookie::get('admin-controls-expanded','open') }}" id="adminControlsSwitch"></button>
         <div class="admin-controls__buttons {{ Cookie::get('admin-controls-expanded','open') }}">
-            @include('voyager::partials.front.button' , 
-                ['button'=> new TCG\Voyager\Helpers\ControlPanel\ControlButton([
-                    'url'=>route('voyager.dashboard'),
-                    'color'=>config('voyager.primary_color'),
-                    'fontColor'=>'white',
-                    'icon'=>'home',
-                    'title'=>'Админ панель'
-                ])])
             @foreach ($buttons as $button)
                 @include('voyager::partials.front.button')
             @endforeach
