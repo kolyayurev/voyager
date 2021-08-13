d07b2cbea5229b3e68306719d2b1678ee90eae8d
kolyayurev <kolyayurew@gmail.com>
2021-08-13 12:07:54 +0300

    remove console log

--- resources/assets/js/front.js
+++ resources/assets/js/front.js
@@ -6,7 +6,6 @@ if(adminControlsSwitch)
     adminControlsSwitch.addEventListener("click",function(e){
         var buttons = document.getElementsByClassName('admin-controls__buttons')[0];
         buttons.classList.contains('open')?Cookies.set('admin-controls-expanded',''):Cookies.set('admin-controls-expanded','open');
-        console.log(buttons.classList.contains('open')?'y':'no');
         buttons.classList.toggle("open");
         this.classList.toggle("open");
     },false);
