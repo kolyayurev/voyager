ae0a682afaf99131fc72f461dde1fc309253c501
kolyayurew <kolyayurew@gmail.com>
2021-10-02 22:05:39 +0300

    added translatable to HasSeo

--- src/Traits/HasSeo.php
+++ src/Traits/HasSeo.php
@@ -2,22 +2,33 @@
 
 namespace TCG\Voyager\Traits;
 
+use Voyager;
+
 trait HasSeo
 {
     public function getH1()
     {
-        return $this->h1??$this->name??'';
+        if(Voyager::translatable($this))
+            return $this->getTranslatedAttribute('h1')??$this->getTranslatedAttribute('name')??'';
+        else
+            return $this->h1??$this->name??'';
     }
     public function getMetaTitle()
     {
-        return $this->meta_title??$this->getH1()??setting('seo.meta_title')??'';
+        return (Voyager::translatable($this)?$this->getTranslatedAttribute('meta_title'):$this->meta_title)??$this->getH1()??setting('seo.meta_title')??'';
     }
     public function getMetaDescription()
     {
-       return  $this->meta_description??$this->excerpt??setting('seo.meta_description')??'';
+        if(Voyager::translatable($this))
+            return $this->getTranslatedAttribute('meta_description')??$this->getTranslatedAttribute('excerpt')??setting('seo.meta_description')??'';
+        else
+            return  $this->meta_description??$this->excerpt??setting('seo.meta_description')??'';
     }
     public function getKeywords()
     {
-        return $this->meta_keywords??$this->getH1()??setting('seo.meta_keywords')??'';
+        if(Voyager::translatable($this))
+            return $this->getTranslatedAttribute('meta_keywords')??$this->getH1()??setting('seo.meta_keywords')??'';
+        else
+            return $this->meta_keywords??$this->getH1()??setting('seo.meta_keywords')??'';
     }
 }
