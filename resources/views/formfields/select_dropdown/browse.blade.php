080bdb423efb48aab34f3a0cb963cc2ecf476ff6
kolyayurew <kolyayurew@gmail.com>
2021-10-12 17:33:31 +0300

    save

--- resources/views/formfields/select_dropdown/browse.blade.php
+++ resources/views/formfields/select_dropdown/browse.blade.php
@@ -1,3 +1,3 @@
 @if (property_exists($row->details, 'options'))
-{!! $row->details->options->{$dataTypeContent->{$row->field}} ?__($row->details->options->{$dataTypeContent->{$row->field}}): '' !!}
+{!! __($row->details->options->{$dataTypeContent->{$row->field}} ?? '') !!}
 @endif
--- resources/views/formfields/select_dropdown/read.blade.php
+++ resources/views/formfields/select_dropdown/read.blade.php
@@ -1,4 +1,4 @@
 @if(property_exists($row->details, 'options') && !empty($row->details->options->{$dataTypeContent->{$row->field}}))
     @include('voyager::multilingual.input-hidden-bread-read')
-    {!! $row->details->options->{$dataTypeContent->{$row->field}} ?__($row->details->options->{$dataTypeContent->{$row->field}}): '' !!}
+    {!!  __($row->details->options->{$dataTypeContent->{$row->field}} ?? '') !!}
 @endif
