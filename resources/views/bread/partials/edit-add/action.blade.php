120cc60ea0d28722a5e3cbe7773f15513dde52f0
kolyayurew <kolyayurew@gmail.com>
2021-10-25 19:21:08 +0300

    save

--- resources/views/bread/partials/edit-add/action.blade.php
+++ resources/views/bread/partials/edit-add/action.blade.php
@@ -4,9 +4,11 @@
         $class = get_class($action);
         $action = new $class($dataType, $dataTypeContent);
     @endphp
-    @can ($action->getPolicy(),$dataTypeContent)
-        <a href="{{ $action->getRoute($dataType->name) }}" title="{{ $action->getTitle() }}" {!! $action->convertAttributesToHtml() !!}>
-            <i class="{{ $action->getIcon() }}"></i> <span class="hidden-xs hidden-sm">{{ $action->getTitle() }}</span>
-        </a>
-    @endcan
+    @if ($action->shouldDisplay())
+        @can ($action->getPolicy(),$dataTypeContent)
+            <a href="{{ $action->getRoute($dataType->name) }}" title="{{ $action->getTitle() }}" {!! $action->convertAttributesToHtml() !!}>
+                <i class="{{ $action->getIcon() }}"></i> <span class="hidden-xs hidden-sm">{{ $action->getTitle() }}</span>
+            </a>
+        @endcan
+    @endif
 @endif
