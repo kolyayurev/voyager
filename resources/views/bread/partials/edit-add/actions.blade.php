95f712aee6ee89f530629e7019b99054a1c68b5e
kolyayurew <kolyayurew@gmail.com>
2021-11-01 17:36:33 +0300

    hot fix

--- resources/views/bread/partials/edit-add/actions.blade.php
+++ resources/views/bread/partials/edit-add/actions.blade.php
@@ -2,11 +2,13 @@
     <x-voyager::panel  :header="false" >
         @section('submit-buttons')
             <button type="submit" class="btn btn-primary save">{{ __('voyager::generic.save') }}</button>
-            @foreach($actions as $action)
-                @if (!method_exists($action, 'massAction'))
-                    @include('voyager::bread.partials.edit-add.action', ['action' => $action])
-                @endif
-            @endforeach
+            @if (!empty($actions))
+                @foreach($actions as $action)
+                    @if (!method_exists($action, 'massAction'))
+                        @include('voyager::bread.partials.edit-add.action', ['action' => $action])
+                    @endif
+                @endforeach
+            @endif
         @stop
         @yield('submit-buttons')
     </x-voyager::panel>
